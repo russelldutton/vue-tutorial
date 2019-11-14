e89ac6a8265a50214850b721efe992a5a0a6ebf5
Russell <rdutton1504@gmail.com>
2019-11-14 20:08:38 +0200

    Created component

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let vue = new Vue({
+var post = Vue.component('post', {
+	data: function() {
+		return {
+			title: 'This is a title',
+			body: 'Just a little more text, nothing much though.'
+		}
+	}
+});
+
+
+var app = new Vue({
 	el: '#app',
 	data: {
 		name: "",
