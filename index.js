60c1b08544e77fd46393311fd8b925b2ab903c27
Russell <rdutton1504@gmail.com>
2019-11-14 21:19:09 +0200

    List render components

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ Vue.component('post', {
 	template: '<div class="post"><h2>{{title}}</h2><p>{{body}}</p><p>{{votes}} votes.</p></div>'
 });
 
-var image = {
-	props: ['url', 'text', 'dims'],
-	template: '<div><img :src="url" :alt="text" :width="dims[0]" :height="dims[1]"/></div>'
-}
-
 
 var app = new Vue({
 	el: '#app',
-	components: {
-		'custom-picture': image
+	data: {
+		posts: 3
+	},
+	computed: {
+		numPosts: function() {
+			var list = [];
+			for (let i = 1; i <= this.posts; i++) {
+				list = [...list, i]
+			}
+			return list;
+		}
 	}
 });
