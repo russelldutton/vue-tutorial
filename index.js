8bdbca330e525478a38494b2673c3006028a32f2
Russell <rdutton1504@gmail.com>
2019-11-14 21:05:16 +0200

    Added local registered component

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var post = Vue.component('post', {
+Vue.component('post', {
+	props: ["votes"],
 	data: function() {
 		return {
 			title: 'This is a title',
 			body: 'Just a little more text, nothing much though.'
 		}
 	},
-	template: '<div class="post"><h2>{{title}}</h2><p>{{body}}</p>'
+	template: '<div class="post"><h2>{{title}}</h2><p>{{body}}</p><p>{{votes}} votes.</p></div>'
 });
+
+var image = {
+	props: ['url', 'text', 'dims'],
+	template: '<div><img :src="url" :alt="text" :width="dims[0]" :height="dims[1]"/></div>'
+}
 
 
 var app = new Vue({
 	el: '#app',
-	data: {
-		name: "",
-	},
-	computed: {
-		valid: function() {
-			return this.name != '';
-		}
+	components: {
+		'custom-picture': image
 	}
 });
